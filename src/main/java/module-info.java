aeac05177996198a396f13c20f57bf87ab5407fc
Bartu Hacılar <121451753+BartuHacilar@users.noreply.github.com>
2023-03-15 18:21:04 +0300

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.ce26projecttest {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.ce26projecttest to javafx.fxml;
-    exports com.example.ce26projecttest;
-}
