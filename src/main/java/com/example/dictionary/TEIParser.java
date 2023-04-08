5b722c18bb6de4a734d29ff558c994e372d610a0
Phaverose <95944108+Phaverose@users.noreply.github.com>
2023-04-08 22:39:42 +0300

    Working Prototype 1

--- src/main/java/com/example/dictionary/TEIParser.java
+++ src/main/java/com/example/dictionary/TEIParser.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 public class TEIParser {
 
     public static HashMap<String,String> Parsing(int Selected) {
+
         String[] fileNames = {
                 "deu-ell.tei","deu-eng.tei","deu-fra.tei","deu-swe.tei","ell-eng.tei",
                 "ell-fra.tei","ell-ita.tei","tur-deu.tei","tur-eng.tei","eng-ell.tei",
@@ -15,17 +16,14 @@ public class TEIParser {
                 "ita-deu.tei","ita-ell.tei","ita-eng.tei","ita-swe.tei","ita-tur.tei",
                 "swe-deu.tei","swe-eng.tei","swe-fra.tei","swe-ita.tei","swe-tur.tei",
                 "eng-ita.tei","deu-tur.tei","deu-ita.tei"};
-        HashMap<String, String>[] maps = new HashMap[33];
-        for (int i = 0; i < 33; i++) {
-            maps[i] = new HashMap<>();
-        }
+        HashMap<String, String> maps  = new HashMap<>();
 
-        try {
+            try {
             // Define file names and corresponding HashMaps
 
             // Loop through files and read data into HashMaps
-            for (int i = 0; i < 33; i++) {
-                String fileName = fileNames[i];
+
+                String fileName = fileNames[Selected];
                 System.out.println(TEIParser.class.getClassLoader().getResource(fileName));
                 InputStream inputStream = TEIParser.class.getClassLoader().getResourceAsStream(fileName);
                 BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
@@ -34,18 +32,20 @@ public class TEIParser {
                 String line;
                 while ((line = reader.readLine()) != null) {
 
-                        if (line.contains("<orth>")) {
+                        if (line.contains("</orth>")) {
                             currentOrth = line.replaceAll("<.*?>", "").trim();
-                        } else if (line.contains("<quote>")) {
+                        } else if (line.contains("</quote>")||line.contains("</def>")) {
                             String quote = line.replaceAll("<.*?>", "").trim();
-                            maps[i].put(currentOrth, quote);
+                            maps.put(currentOrth, quote);
+                            if(maps.keySet()==null)
+                                System.out.println("this hashmap is empty");
                             currentOrth = null;
 
                     }
                 }
 
                 reader.close();
-            }
+
 
 
 
@@ -54,7 +54,7 @@ public class TEIParser {
         } catch (Exception e) {
             e.printStackTrace();
         }
-    return maps[Selected];
+    return maps;
 
     }
 
