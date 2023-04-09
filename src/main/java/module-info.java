90fafb889237fb47cb9e85ca9e7505890b0a707d
SurceX <85957493+SurceX@users.noreply.github.com>
2023-04-09 23:03:29 +0300

    Last updates, and executable .jar file

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.dictionary {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.xml;
-
-
-    opens com.example.dictionary to javafx.fxml;
-    exports com.example.dictionary;
-}
